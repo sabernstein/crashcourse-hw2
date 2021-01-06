cf566082889b303a7e2b67aefb0972db2c151bf9
sabernstein <65577560+sabernstein@users.noreply.github.com>
2021-01-06 10:07:35 -0800

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import Note from "./Note";
 import notes from "../notes";
 
 function App() {
   return (
     <div>
       <h1> Keeper </h1>
-      {notes.map(function(note)
+      {notes.map(function(note) {
         return(
           <Note
             title={note.title}
             content={note.content}
           />
         );
-      }
+      })}
       <p> Copyright © 2021 </p>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Note from "./Note";
-import notes from "../notes"
 
 function Note(props){
   return(
     <div>
-      <h1 >{props.title}</h1>
+      <h1>{props.title}</h1>
       <p>{props.content}</p>
     </div>
   );
